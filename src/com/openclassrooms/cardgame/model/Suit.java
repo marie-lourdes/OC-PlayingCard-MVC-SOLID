ddba5888222e688e5475f2d8125a38727488e80e
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-01 12:31:45 +0200

    fix branch

--- src/com/openclassrooms/cardgame/model/Suit.java
+++ src/com/openclassrooms/cardgame/model/Suit.java
@@ -1,13 +1,7 @@
 package com.openclassrooms.cardgame.model;
 
 public enum Suit {
-	//carte Jocker sans couleur au lieu de passer par l heritage de playingCard qui ne prevoyez pas de carte sans valeur et couleur
-	//on modifie l enum rank et suit en ajoutant la valeur 0 pour les valeur rank et couleur suit
-	NONE(0)
-	DIAMONDS(1),
-	HEARTS(2),
-	SPADES(3),
-	CLUBS(4);
+	DIAMONDS(1), HEARTS(2), SPADES(3), CLUBS(4);
 
 	int suit;
 
