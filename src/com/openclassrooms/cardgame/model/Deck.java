9d35a65d8e4be419d6a45b359170d4f9140494a9
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-01 15:10:29 +0200

    Deck.java: add condition   to select one ranck  value and color suit none for Jocker card  to add in deck cards loops

--- src/com/openclassrooms/cardgame/model/Deck.java
+++ src/com/openclassrooms/cardgame/model/Deck.java
@@ -7,30 +7,40 @@ import java.util.Random;
 
 public class Deck {
 	private List<PlayingCard> cards;
-	
+
 	public Deck() {
 		cards = new ArrayList<PlayingCard>();
-		for(Rank rank : Rank.values()) {
-			for(Suit suit: Suit.values()) {
-				System.out.println("Creating card ["+rank+"]["+suit+"]");
-				cards.add(new PlayingCard(rank, suit));
+		for (Rank rank : Rank.values()) {
+
+			for (Suit suit : Suit.values()) {
+				if (rank.value() == 0 && suit.value() == 0) {
+					System.out.println("Creating card [" + rank + "][" + suit + "]");
+					cards.add(new PlayingCard(rank, suit));
+					break;
+				}
+				if (suit.value() != 0) {
+					System.out.println("Creating card [" + rank + "][" + suit + "]");
+					cards.add(new PlayingCard(rank, suit));
+				}
+
 			}
+
 		}
-		
+
 		shuffle();
 	}
 
 	public void shuffle() {
 		Random random = new Random();
 		for (int i = 0; i < cards.size(); ++i) {
-			Collections.swap(cards, i, random.nextInt (cards.size()));
+			Collections.swap(cards, i, random.nextInt(cards.size()));
 		}
 	}
-	
+
 	public PlayingCard removeTopCard() {
 		return cards.remove(0);
 	}
-	
+
 	public void returnCardToDeck(PlayingCard pc) {
 		cards.add(pc);
 	}
@@ -38,5 +48,5 @@ public class Deck {
 	public List<PlayingCard> getCards() {
 		return cards;
 	}
-	
+
 }
--- src/com/openclassrooms/cardgame/model/Suit.java
+++ src/com/openclassrooms/cardgame/model/Suit.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.cardgame.model;
 
 public enum Suit {
-	DIAMONDS(1), HEARTS(2), SPADES(3), CLUBS(4);
+	// ajout de none value 0 pour la carte Joker qui n a pas de valeur rank et de
+	// couleur suit
+	NONE(0), DIAMONDS(1), HEARTS(2), SPADES(3), CLUBS(4);
 
 	int suit;
 
