f403c83f82e68d74a4b96a7aa8d490e12f1d44aa
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-09-05 13:15:15 +0200

    add comments in games.java

--- src/com/openclassrooms/cardgame/Games.java
+++ src/com/openclassrooms/cardgame/Games.java
@@ -4,6 +4,7 @@ import com.openclassrooms.cardgame.buider.GameBuilder;
 import com.openclassrooms.cardgame.buider.NormalHightCardGameBuilder;
 import com.openclassrooms.cardgame.controller.GameController;
 import com.openclassrooms.cardgame.model.Game;
+import com.openclassrooms.cardgame.model.SomeClass;
 import com.openclassrooms.cardgame.view.GameSwingView;
 
 public class Games {
@@ -12,6 +13,10 @@ public class Games {
 
 		GameSwingView gsv = new GameSwingView();
 		gsv.createAndShowGUI();
+
+		/* ****************************pattern Factory****************************** */
+
+		// design pattern Factory
 		// appel de la methode makedeck a partir de la class Deckfactory sans l
 		// instancier car c est une classe utilitaire avec sa methode static
 		// DeckFactory se charge d instancier la classe de type deck selon le type:
@@ -22,6 +27,9 @@ public class Games {
 		 * GameController gc = new GameController(DeckFactory.makeDeck(DeckType.NORMAL),
 		 * gsv, GameEvaluatorFactory.makeEvaluator(EvaluatorType.HIGHT));
 		 */
+
+		/* *****************************pattern Buider*************************** */
+
 		// design pattern Buider qui permet de recuperer une formule de jeu avec l
 		// option small jeu 32 cartes et l evaluation de la carte la plus forte
 		// Et lancer le leu avec ces options precise et assemblée ensemble
@@ -30,22 +38,29 @@ public class Games {
 		GameController gc = new GameController(game.getDeck(), gsv, game.getEvaluator());
 		gc.run();
 
-		// design pattern prototype clone pour recreer des objets avec copie et un clone
+		/*
+		 * *******************************pattern Prototype***************************
+		 */
+
+		// design pattern Prototype clone pour recreer des objets avec copie et un clone
 		// nouvel instance
 		// pour eviter de consommer tropde memeoire lorsqu il a plusieur objets a
 		// intancier, permet de ne pas creer plusieir instances avec new et des valeurs
 		// differentes mais de cloner un objet
 		// et modifier les valeur du clone de l objets
 
-		/*
-		 * SomeClass a = new SomeClass(); a.someField = 1202; a.someField2 = 5456;
-		 * System.out.println(a.someField); SomeClass b = (SomeClass) a.clone();
-		 * 
-		 * // a.someField remains 1202 // since it is a different object
-		 * 
-		 * b.someField = 5; System.out.println(b.someField);
-		 * System.out.println(a.someField); System.out.println(b.someField2);
-		 */
+		SomeClass a = new SomeClass();
+		a.someField = 1202;
+		a.someField2 = 5456;
+		System.out.println(a.someField);
+		SomeClass b = (SomeClass) a.clone();
+
+		// a.someField remains 1202 // since it is a different object
+
+		b.someField = 5;
+		System.out.println(b.someField);
+		System.out.println(a.someField);
+		System.out.println(b.someField2);
 
 	}
 
